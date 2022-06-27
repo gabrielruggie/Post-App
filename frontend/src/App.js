d5e5dbefd23aa51ae86956d7ea08e3d82b3583d7
gabrielruggie <69177531+gabrielruggie@users.noreply.github.com>
2022-06-26 22:27:53 -0500

    started designing homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App w-full">
+      <div className='text-center text-8xl font-bold font-mono text-yellow-500 pt-20 my-10'>POST APP</div>
+      <ul className='flex space-x-3 flex-wrap items-center justify-center'>
+        <li className='p-10 bg-yellow-500 rounded-md text-xl font-mono font-bold self-auto'>Sign In</li>
+        <li className='p-10 bg-yellow-500 rounded-md text-xl font-mono font-bold self-auto'>Register Today</li>
+        <li className='p-10 bg-yellow-500 rounded-md text-xl font-mono font-bold self-auto'>About Post App</li>
+      </ul>
+    </div>  
   );
 }
 
