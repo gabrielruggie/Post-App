4edb3b86e7cd5f12609e03a10ccd21e0a6acc3ef
gabrielruggie <69177531+gabrielruggie@users.noreply.github.com>
2022-06-29 22:21:04 -0500

    started designing registration page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className='text-center text-8xl font-bold font-mono text-yellow-500 pt-20 my-10'>POST APP</div>
       <ul className='flex space-x-3 flex-wrap items-center justify-center'>
         <li className='p-10 bg-yellow-500 rounded-md text-xl font-mono font-bold self-auto'>
-          <Link to="/login" onClick={changeBackgroundColor}>Login</Link>  
+          <Link to="/login">Login</Link>  
         </li>
         <li className='p-10 bg-yellow-500 rounded-md text-xl font-mono font-bold self-auto'>
           <Link to="/register">Register</Link>  
--- frontend/src/routes/Register.jsx
+++ frontend/src/routes/Register.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 
 export default function Register() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmission (event) {
+    event.preventDefault();
+
+    // Check fields here
+
+    
+
+  }
+
   return (
-    <div>This is the registration page</div>
+    <div>
+      <form className='grid justify-center text-center' onSubmit={handleSubmission}>
+        <h1>Register</h1>
+        
+        <div className=''>
+          <label htmlFor="firstName">Enter Your First Name</label>
+          <input 
+            type="text" 
+            name="firstName"
+            value={firstName}
+            onChange={(event) => setFirstName(event.target.value)}
+            className="w-full"
+          />
+        </div>
+
+        <label htmlFor="lastName">Enter Your Last Name</label>
+        <input 
+          type="text" 
+          name="lastName"
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+
+        <label htmlFor="username">Enter Your Desired Username</label>
+        <input 
+          type="text" 
+          name="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+
+        <label htmlFor="email">Enter Your Email Address</label>
+        <input 
+          type="text" 
+          name="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+
+        <label htmlFor="password">Enter Your Password</label>
+        <input 
+          type="text" 
+          name="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <button>Submit</button>
+
+      </form>
+    </div>
   )
 }
