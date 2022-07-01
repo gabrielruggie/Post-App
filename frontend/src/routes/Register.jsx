08e0f14a1abac2901f2cea4f5f37d709ce262f82
gabrielruggie <69177531+gabrielruggie@users.noreply.github.com>
2022-06-30 22:59:39 -0500

    added css to registration page

--- frontend/src/routes/Register.jsx
+++ frontend/src/routes/Register.jsx
@@ -12,59 +12,69 @@ export default function Register() {
 
     // Check fields here
 
-    
-
   }
 
   return (
-    <div>
-      <form className='grid justify-center text-center' onSubmit={handleSubmission}>
-        <h1>Register</h1>
+    <div className='grid justify-center text-center my-20'>
+      <h1>Register</h1>
+      <form className='my-20 space-y-10' onSubmit={handleSubmission}>
         
-        <div className=''>
-          <label htmlFor="firstName">Enter Your First Name</label>
+        <div>
+          <label className='font-mono text-2xl text-yellow-400 font-bold' htmlFor="firstName">Enter Your First Name</label>
           <input 
             type="text" 
             name="firstName"
             value={firstName}
             onChange={(event) => setFirstName(event.target.value)}
-            className="w-full"
+            className="w-full rounded-md text-2xl font-mono"
           />
         </div>
 
-        <label htmlFor="lastName">Enter Your Last Name</label>
-        <input 
-          type="text" 
-          name="lastName"
-          value={lastName}
-          onChange={(event) => setLastName(event.target.value)}
-        />
+        <div>
+          <label className='font-mono text-2xl text-yellow-400 font-bold' htmlFor="lastName">Enter Your Last Name</label>
+          <input 
+            type="text" 
+            name="lastName"
+            value={lastName}
+            onChange={(event) => setLastName(event.target.value)}
+            className="w-full rounded-md text-2xl font-mono"
+          />
+        </div>
 
-        <label htmlFor="username">Enter Your Desired Username</label>
-        <input 
-          type="text" 
-          name="username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
+        <div>
+          <label className='font-mono text-2xl text-yellow-400 font-bold' htmlFor="username">Enter Your Desired Username</label>
+          <input 
+            type="text" 
+            name="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            className="w-full rounded-md text-2xl font-mono"
+          />
+        </div>
 
-        <label htmlFor="email">Enter Your Email Address</label>
-        <input 
-          type="text" 
-          name="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
+        <div>
+          <label className='font-mono text-2xl text-yellow-400 font-bold' htmlFor="email">Enter Your Email Address</label>
+          <input 
+            type="text" 
+            name="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            className="w-full rounded-md text-2xl font-mono"
+          />
+        </div>
 
-        <label htmlFor="password">Enter Your Password</label>
-        <input 
-          type="text" 
-          name="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
+        <div>
+          <label className='font-mono text-2xl text-yellow-400 font-bold' htmlFor="password">Enter Your Password</label>
+          <input 
+            type="text" 
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className="w-full rounded-md text-2xl font-mono"
+          />
+        </div>
 
-        <button>Submit</button>
+      <button>Submit</button>
 
       </form>
     </div>
