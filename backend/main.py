23beebc1e9966b5bcc20f05df9e970e886cf5a2e
gabrielruggie <69177531+gabrielruggie@users.noreply.github.com>
2022-07-10 21:14:28 -0500

    removed login backend route as it is no longer needed

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 # Routers
 from routes.register_routes import register
 from routes.dashboard_routes import dashboard
-from routes.login_routes import login
 from routes.security_routes import security
 
 origins =['http://localhost:3000']
@@ -17,7 +16,6 @@ def setup ():
 
     app.include_router(register, prefix="/register", tags=["register"])
     app.include_router(dashboard, prefix="/dashboard", tags=["dashboard"])
-    app.include_router(login, prefix="/login", tags=["login"])
     app.include_router(security, prefix="/login", tags=["login"])
 
     # Allowing communication between frontend and backend
